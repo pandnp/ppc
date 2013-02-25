5568f902d662f557debb0b8492b93ecc740789fe
Fionnuala Gunter <fin@linux.vnet.ibm.com>
2013-02-25 14:19:36 -0600

    compiling and using shared libraries

--- function_caller.c
+++ function_caller.c
@@ -1,5 +1,22 @@
 /*
+ * Compiling factorial_caller.c
  * gcc -m64 factorial.s factorial_caller.c -o factorial
+ * ./factorial
+ */
+
+/*
+ * Compiling shared libraries
+ * gcc -m64 -shared factorial.s my_square.s -o libmymath.so
+ */
+
+/*
+ * Using the shared library
+ * gcc -m64 factorial caller.c -o factorial -L. -lmymath
+ *
+ * Tell the dynamic linker what additional directories to search
+ * export LD_LIBRARY_PATH=.
+ *
+ * Run the program
  * ./factorial
  */
 
